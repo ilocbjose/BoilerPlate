04c8f770223c8b3cb0eb688db97971d3903db898
Joseba Urizarbarrena <jurizarbarrenahontoria@gmail.com>
2020-10-19 18:44:13 +0100

    Initial Commit

---
+++ boilerplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  boilerplate
+//
+//  Created by user176708 on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
